701fb387b3939b766a2198b67bdc24965cfaa904
Valente FV <x95836@gmail.com>
2020-03-16 11:28:31 +0000

    [update] update and delete profile working

--- crudago/crudago/settings.py
+++ crudago/crudago/settings.py
@@ -143,7 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    '/update_profile/static',
+)
 
 
 LOGIN_URL = 'login'
